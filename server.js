6258c54047b76fe26f4ab2fff93491449287eae8
PrajaktaRpatil02 <patilprajaktar2@gmail.com>
2022-07-17 13:56:45 +0530

    elearning

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-
 var express = require('express');
 var app = express();
-var path=require("path");
-var fs=require("fs");
+var path = require("path");
+var fs = require("fs");
 
 //server configuration
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-   res.sendFile(path.join(___dirname + '/index.html'));
+    res.sendFile(path.join(___dirname + '/index.html'));
 });
- 
+
 
 app.get('/hello', function (req, res) {
-  console.log("CAlling rest api");
-  var person={firstName:'Ravi',lastName:'Tambade',age:47};
-  res.send(person);
+    console.log("CAlling rest api");
+    var person = { firstName: 'Ravi', lastName: 'Tambade', age: 47 };
+    res.send(person);
 });
 
- 
+
 var server = app.listen(8081, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log("Example app listening at 8081");
+    console.log("Example app listening at 8081");
 })
